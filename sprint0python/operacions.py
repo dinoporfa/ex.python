48fff75e04949afa399945b05373112ebcd1dff0
lois <lfajardot@fpcoruna.org>
2024-10-09 10:49:20 +0200

    ex1

---
+++ sprint0python/operacions.py
@@ -0,0 +1,15 @@
+def suma(num1, num2):
+    return num1 + num2
+
+def resta(num1, num2):
+    return num1 - num2
+
+def multi(num1, num2):
+    return num1 * num2
+
+def divi(num1, num2):
+    if num2==0:
+        print('non se pode dividir entre 0')
+        return 0
+    else:
+        return num1 / num2
